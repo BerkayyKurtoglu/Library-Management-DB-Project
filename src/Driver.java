933cca3b156f2cb88eb6fd5672cf812c97691056
Berkay Kurtoglu <berkayberkay300@hotmail.com>
2022-04-29 18:32:20 +0300

    Some sql connection test

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,28 @@
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Driver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = MariaDBConnection.getMariaDbConnection();
+
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `library management system`.manager");
+            while (resultSet.next()) {
+                System.out.println(
+                                resultSet.getString("man_name")+" "
+                                +resultSet.getString("man_tc")+" "
+                                +resultSet.getString("man_department")
+                );
+            }
+        }else {
+            System.out.println("Connection failed");
+        }
 
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
---
+++ src/MariaDBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MariaDBConnection {
+
+    public static Connection getMariaDbConnection() {
+
+        try {
+            String MARIA_DB_CONNECTION_URL = "jdbc:mariadb://localhost:1903/library management system?user=root&password=root";
+            return DriverManager.getConnection(MARIA_DB_CONNECTION_URL);
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }
+
+
+}
